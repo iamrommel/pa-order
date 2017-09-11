de560b3c6f4f3d53818b4fddd12897e1960e7bf5
Rommel Manalo <manalo.rommel@gmail.com>
2017-09-11 21:59:49 +0800

    Added the web-server and web-client

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 
 class App extends Component {
   render() {
-
-    console.log('some shit happens')
-
     return (
       <div className="App">
         <div className="App-header">
