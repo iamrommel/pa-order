3f433a3497f234fcf6f2ceaaba01ba1c1e04ec34
Rommel Manalo <manalo.rommel@gmail.com>
2017-09-12 01:26:28 +0800

    added the nodemon

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.json([{name: 'Rommel', age: 32}])
+  res.json([{name: 'Rommel and the house', age: 32}])
 })
 
 module.exports = router
