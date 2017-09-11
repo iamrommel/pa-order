ef4ca5c1d40b9851463fe8e2851fbc35beba6d45
Rommel Manalo <manalo.rommel@gmail.com>
2017-09-11 21:46:50 +0800

    Added the client part

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    console.log('some shit happens')
+
     return (
       <div className="App">
         <div className="App-header">
