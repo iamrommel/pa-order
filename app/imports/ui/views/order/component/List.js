252658db1205982dd3c0121cb5f3e1bb36769a8a
Rommel Manalo <manalo.rommel@gmail.com>
2017-09-28 22:27:26 +0800

    items

--- app/imports/ui/views/order/component/List.js
+++ app/imports/ui/views/order/component/List.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Utils } from 'pcmli.umbrella.uni-core'
-import { Label } from 'pcmli.umbrella.web-ui'
+import { Label, Table, Tags } from 'pcmli.umbrella.web-ui'
 import WebUi from 'pcmli.umbrella.web-ui'
 import Core from 'pcmli.umbrella.core'
 
 import { listQueryConfig } from '../../../services/order'
 import { routesDef } from '../../../../startup/client/config/routes'
-import { CustomerDisplay } from '../../../components'
 import { OrderStatusLabel } from './OrderStatusLabel'
 
 let TableRow = (props) => {
@@ -19,26 +18,31 @@ let TableRow = (props) => {
 
   return (
     <tr>
-
       <td style={{width: '8%'}}>
-        <Link to={`${routesDef.CustomersDetailPage.altPath}${_id}/${code}`}><h3>#{code}</h3></Link>
+        <Link to={`${routesDef.CustomersDetailPage.altPath}${_id}/${code}`}>#{code}</Link>
+      </td>
+      <td>
+        <h4>
+          {Utils.formatNumber(netAmount, 'Php 0,0.00')}
+        </h4>
       </td>
       <td>
         {Utils.formatDateTime(timeStamp)}
       </td>
       <td>
-        <CustomerDisplay value={props}/>
-      </td>
-      <td>
         <OrderStatusLabel status={status}/>
       </td>
       <td>
-        <WebUi.Tags dataArray={tags}/>
-      </td>
-      <td style={{width: '15%'}}>
-        <small className="font-italic">{remarks}</small>
+        <p>
+          <Label type="default"><strong>{details.length || 0}</strong></Label> items
+        </p>
       </td>
 
+      <td>
+        <Tags dataArray={tags}/>
+        <br/>
+        <small className="font-italic">{remarks}</small>
+      </td>
     </tr>
   )
 }
@@ -53,15 +57,16 @@ let List = ({loadMoreEntries, items, searchString, sortObject, pageInfo, loading
 
   let result = (
 
-    <WebUi.Table
+    <Table
       dataArray={items}
       createRow={createRow}
-      stats="customers"
+      stats="orders"
       pageInfo={pageInfo}
       loading={loading}
       loadMoreEntries={loadMoreEntries}
       searchString={searchString}
       sortObject={sortObject}
+      className="table-space-small"
     />
   )
 
