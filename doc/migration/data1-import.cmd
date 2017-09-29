f7da189a203d5f9d6ea4b2c3acbc5476335c5050
Rommel Manalo <manalo.rommel@gmail.com>
2017-09-29 12:27:34 +0800

    remvoed the migration

--- doc/migration/data1-import.cmd
+++
@@ -1,2 +0,0 @@
-mongoimport --host localhost:27017 --db local-dev --collection customers --file data1.json --upsert
-mongoimport --host localhost:3001 --db meteor --collection customers --file data1.json --upsert
